25bc14084cb1e64359abcd8bcd091dab8a6862cb
troychaplin <troy.chaplin@gmail.com>
2025-02-28 22:44:32 -0500

    finish: project template base

--- src/templates/index.js
+++ src/templates/index.js
@@ -14,19 +14,30 @@ module.exports = {
 				requiresAtLeast: isInteractiveVariant ? '6.5' : '6.1',
 			};
 		},
-		version: '1.0.0',
+		version: '0.1.0',
 		namespace: 'multi-block-of-madness',
 		description:
 			'A plugin that brings a collection of blocks and related functionality to the WordPress block editor.',
-		render: 'file:./render.php',
+		category: 'widgets',
+		textdomain: 'multi-block-of-madness',
+		editorScript: 'file:./index.js',
+		editorStyle: 'file:./index.css',
+		style: 'file:./style-index.css',
 	},
 	variants: {
-		dynamic: {},
+		dynamic: {
+			render: 'file:./render.php',
+		},
 		interactive: {
 			viewScriptModule: 'file:./view.js',
 			supports: {
 				interactive: true,
 			},
+		},
+		static: {
+			customBlockJSON: {
+				render: null
+			}
 		},
 	},
 	blockTemplatesPath: join(__dirname, 'files'),
