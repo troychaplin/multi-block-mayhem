7bf8cd8d49f0a7fcb1cc18e2005bfa5bc94afd4a
troychaplin <troy.chaplin@gmail.com>
2025-03-07 16:42:23 -0500

    change: cleaned up editor css

--- build/blocks/image/mosaic-gallery/index.asset.php
+++ build/blocks/image/mosaic-gallery/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => '3d1f8940657bfd8ff2cf');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'e6913859dbc3f1c6f6b3');
