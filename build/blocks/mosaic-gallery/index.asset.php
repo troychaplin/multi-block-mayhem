02eee9cd15685c3d4565076229c848abfba7ca13
Troy Chaplin <troy.chaplin@gmail.com>
2025-10-04 11:02:11 -0400

    fix: image column flow

--- build/blocks/mosaic-gallery/index.asset.php
+++ build/blocks/mosaic-gallery/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'd97fe4175e1e49b02b48');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-compose', 'wp-data', 'wp-hooks', 'wp-i18n', 'wp-primitives'), 'version' => 'cabe8f3c0736e1ddb742');
